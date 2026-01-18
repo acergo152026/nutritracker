49622f7790d1f0dcf1c81f0a591b8f1474529f7b
acergo152026 <acergo152026@hotmail.com>
2026-01-18 22:07:36 +0100

    Create intake.dart

---
+++ lib/models/intake.dart
@@ -0,0 +1,21 @@
+class Intake {
+  final int id;
+  final String name;
+  final double grams;
+  final double calories;
+  final double protein;
+  final double carbs;
+  final double fat;
+  final DateTime date;
+
+  Intake({
+    required this.id,
+    required this.name,
+    required this.grams,
+    required this.calories,
+    required this.protein,
+    required this.carbs,
+    required this.fat,
+    required this.date,
+  });
+}
